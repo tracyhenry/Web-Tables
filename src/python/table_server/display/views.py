ad18355471045363471f9380efd9bc5c8862edff
tracyhenry <thierryhenrytracy@163.com>
2015-03-13 14:56:34 +0800

    Added ground truths.

--- src/python/table_server/display/views.py
+++ src/python/table_server/display/views.py
@@ -22,6 +22,9 @@ def showtable(request):
     # Get the schema of this table
     schemas = TableSchema.objects.filter(table_id = table_id)
 
+    # Ground Truth
+    ground_truths = GoldAnnotation.objects.filter(table_id = table_id)
+
     # Get the url of this table
     table_url = cells[0].table_url
 
@@ -46,7 +49,20 @@ def showtable(request):
     for col in schemas:
         schema_list.append({"col" : col.att_id, "value" : col.att_name})
 
+    # Ground Truth Lists
+    gt_list = []
+    for col in ground_truths:
+        gt_list.append({"col" : col.col, "value" : col.gold_type})    
+    for i in range(0, M):
+        appeared = False
+        for col in ground_truths:
+            if col.col == i:
+                appeared = True
+        if appeared == False:
+            gt_list.append({"col" : i, "value" : "NO_GROUND_TRUTH"})
+
     return render(request, 'showtable.html', {'row_list' : row_list,
                                               'table_url' : table_url,
+                                              'gt_list' : gt_list,
                                               'schema_list' : schema_list})
 
