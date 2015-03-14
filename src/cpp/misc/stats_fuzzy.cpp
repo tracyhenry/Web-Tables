5c4ddd676d18e355ad1492a013d4d588853f5430
tracyhenry <thierryhenrytracy@163.com>
2015-03-14 22:00:29 +0800

    Results and simple stats of an initial matching.

---
+++ src/cpp/misc/stats_fuzzy.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+using namespace std;
+
+int matched[1010100];
+
+int main()
+{
+	freopen("../../../data/Table/fuzzy_wwt_yago_0.8_0.8.txt", "r", stdin);
+
+	string s;
+	memset(matched, 0, sizeof(matched));
+
+	while (getline(cin, s))
+	{
+		//line 1
+		stringstream sin1(s);
+		double sim;
+		int entityID, cellID;
+
+		sin1 >> sim >> entityID >> cellID;
+
+		//line 2~4
+		for (int i = 1; i <= 3; i ++)
+			getline(cin, s);
+
+		matched[cellID] ++;
+	}
+
+	int multiMatch = 0;
+	int noMatch = 0;
+	int oneMatch = 0;
+	for (int i = 1; i <= 551351; i ++)
+		if (matched[i] > 1)
+			multiMatch ++;
+		else if (matched[i] == 1)
+			oneMatch ++;
+		else noMatch ++;
+
+	cout << "Number of cells that have more than one matched entity: " << endl << "      " << multiMatch << endl;
+	cout << "Number of cells that have exactly one matched entity: " << endl << "      " << oneMatch << endl;
+	cout << "Number of cells that have no matches: " << endl << "      " << noMatch << endl;
+	return 0;
+}
