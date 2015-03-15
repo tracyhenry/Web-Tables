f090296b9af2a7a3adf83d66e2c9263b64297f5b
tracyhenry <thierryhenrytracy@163.com>
2015-03-15 22:20:52 +0800

    Entity coverage is much higher than attribute coverage.

--- src/cpp/misc/stats_wwt.cpp
+++ src/cpp/misc/stats_wwt.cpp
@@ -28,26 +28,34 @@ struct Cell
 struct Table
 {
 	vector<vector<Cell>> cells;
-	int nRow, nCol, table_id;
+	int nRow, nCol, table_id, entityCol;
 };
 
 
 vector<Cell> allCells;
 vector<Table> allTables;
+vector<string> E;
 
 int nCell = 0;
 int nTable = 0;
 
 int main()
 {
+	ifstream fin0("../../../data/KB/Entities.txt");
 	ifstream fin1("../../../data/Table/wwt_id");
 	ifstream fin2("../../../data/Table/wwt_value");
-	ifstream fin3("../../../data/fuzzy/result_0.8_0.8.txt");
-//	freopen("../../../data/fuzzy/stats_0.8_0.8.txt", "a", stdout);
+	ifstream fin3("../../../data/Table/wwt_isentity");
+	ifstream fin4("../../../data/fuzzy/result_0.8_0.8.txt");
+	freopen("../../../data/fuzzy/stats_0.8_0.8.txt", "a", stdout);
 
 	allCells.clear();
 	allCells.push_back(Cell());
 
+	//File 0
+	E.clear(); E.push_back("");
+	string e;
+	while (getline(fin0, e))
+		E.push_back(e);
 	//File 1
 	int r, c, tid;
 	while (fin1 >> r >> c >> tid)
@@ -56,9 +64,9 @@ int main()
 		cur.row = r, cur.col = c, cur.table_id = tid;
 		cur.id = ++ nCell;
 		cur.matched = 0;
+		cur.value = "";
 		allCells.push_back(cur);
 	}
-//	cout << "Read file 1!" << endl;
 
 	//File 2
 	string value;
@@ -67,11 +75,17 @@ int main()
 		getline(fin2, value);
 		allCells[i].value = value;
 	}
-//	cout << "Read file 2!" << endl;
 
 	//File 3
+	int isentity;
+	map<int, int> entityCol; entityCol.clear();
+	while (fin3 >> c >> isentity >> tid)
+		if (isentity)
+			entityCol[tid] = c;
+
+	//File 4
 	string s;
-	while (getline(fin3, s))
+	while (getline(fin4, s))
 	{
 		//line 1
 		stringstream sin1(s);
@@ -85,19 +99,18 @@ int main()
 
 		//line 2~4
 		for (int i = 1; i <= 3; i ++)
-			getline(fin3, s);
+			getline(fin4, s);
+
 	}
-//	cout << "Read file 3!" << endl;
 
 	//make tables
 	allTables.clear();
 	sort(allCells.begin(), allCells.end());
-	cout << "Sorted table! The number of cells: " << endl << "      " << nCell << endl;
+//	cout << "Sorted table! The number of cells: " << endl << "      " << nCell << endl;
 
 	for (int i = 1; i <= nCell; i ++)
 		if (i == 1 || allCells[i].table_id != allCells[i - 1].table_id)
 		{
-//			cout << i << " " << allTables.size() << endl;
 			Table cur;
 			cur.table_id = allCells[i].table_id;
 			cur.cells.clear();
@@ -127,20 +140,25 @@ int main()
 				cur.cells[x][y] = allCells[j];
 			}
 
+			//entity column
+			if (entityCol.count(cur.table_id))
+				cur.entityCol = entityCol[cur.table_id];
+			else cur.entityCol = -1;
+
 			allTables.push_back(cur);
 		}
 
 	//make some stats
 
 	nTable = allTables.size();
-	cout << "Total number of tables in WWT: " << endl << "      " << nTable << endl;
+//	cout << "Total number of tables in WWT: " << endl << "      " << nTable << endl;
 
 	int nManyCol = 0;
 	for (int i = 0; i < nTable; i ++)
 		if (allTables[i].nCol >= 4)
 			nManyCol ++;
 
-	cout << "Total number of tables that have more than 4 columns: " << endl << "      " << nManyCol << endl;
+//	cout << "Total number of tables that have more than 4 columns: " << endl << "      " << nManyCol << endl;
 
 	int nManyLucky = 0;
 	for (int i = 0; i < nTable; i ++)
@@ -152,11 +170,32 @@ int main()
 				if (allTables[i].cells[x][y].matched)
 					nLucky ++;
 
-		if (nLucky * 2 >= total)
+		if (10 * nLucky >= 3 * total)
 			nManyLucky ++;
 	}
 
-	cout << "Total number of tables that have more than 50% lucky cells: " << endl << "      " << nManyLucky << endl;
+//	cout << "Total number of tables that have more than 30% lucky cells: " << endl << "      " << nManyLucky << endl;
+
+	int maxMatch = 0;
+	for (int i = 1; i <= nCell; i ++)
+		if (allCells[i].value.size() > 0)
+			maxMatch = max(maxMatch, allCells[i].matched);
+//	cout << "The maximum number of entities a table cell can be matched to : " << endl << "      " << maxMatch << endl;
+
+	int nEntity = 0;
+	int matchedEntity = 0;
+	for (int i = 0; i < nTable; i ++)
+	{
+		if (allTables[i].entityCol < 0)
+			continue;
+		int entityCol = allTables[i].entityCol;
+		for (int x = 0; x < allTables[i].nRow; x ++)
+			if (allTables[i].cells[x][entityCol].matched)
+				matchedEntity ++;
+		nEntity += allTables[i].nRow;
+	}
+	cout << endl << "The number of total entities in WWT:" << endl << "      " << nEntity << endl;
+	cout << endl << "The number of matched entities in WWT:" << endl << "      " << matchedEntity  << endl;
 
 	return 0;
 }
